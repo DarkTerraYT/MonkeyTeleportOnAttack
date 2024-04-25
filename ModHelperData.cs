307c7b4077926af93bb93e5d7e0138a96a9364bb
CubedLeaves <105320711+DarkTerraYT@users.noreply.github.com>
2024-04-25 19:41:30 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "Monkeys Can't Decide Where to be";
 
-    public const string Description = "Whenever a tower shoots, it's position changes. \nIf the tower ever gets in a place where it can't shoot there's an ability to \"unstuck\" it.";
+    public const string Description = "Whenever a tower shoots, it's position changes. \nIf the tower ever gets in a place where it can't shoot there's an ability to 'unstuck' it.";
 
     public const string RepoOwner = "DarkTerraYT"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "MonkeysTeleportOnAttack"; // TODO add your repo name here, also in the download url in README.md
