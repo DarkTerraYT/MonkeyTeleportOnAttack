90a4f301e9ec92ecb00b809dc1d4ddc1600f7e05
DarkTerraYT <liamwolfguy@gmail.com>
2024-10-23 22:32:27 -0400

    Update Version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,8 +2,8 @@ namespace MonkeyGoByeByeOnPewPew;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "42.0";
-    public const string Version = "1.0.0";
+    public const string WorksOnVersion = "45.2";
+    public const string Version = "1.0.1";
     public const string Name = "Monkeys Can't Decide Where to be";
 
     public const string Description = "Whenever a tower shoots, it's position changes. \nIf the tower ever gets in a place where it can't shoot there's an ability to 'unstuck' it.";
